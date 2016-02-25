e0a3f473ef1c4e3d9d6e7fb259c36d26b2ab4aa4
Bogdan Hryhorian <carerakjan@gmail.com>
2016-02-26 00:10:36 +0200

    added parser for enctype 'application/x-www-form-urlencoded'

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const MultipartDataParser = require('./lib/MultipartDataParser');
+const UrlencodedDataParser = require('./lib/UrlencodedDataParser');
 
 module.exports = function(request){
 
     var contentType = String(request.headers['content-type']);
 
     if(~contentType.indexOf('application/x-www-form-urlencoded')) {
-
+        return new UrlencodedDataParser(request);
     }
 
     if(~contentType.indexOf('multipart/form-data')) {
